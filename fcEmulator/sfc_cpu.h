d1a777c9cf82fd8c431047c931d94e54c5a8958d
eclair-lumiere <1164983144l@gmail.com>
2019-05-23 00:40:52 +0800

    Update sfc_cpu.h

--- fcEmulator/sfc_cpu.h
+++ fcEmulator/sfc_cpu.h
@@ -1,7 +1,60 @@
 #pragma once
-
+// <summary>
+// nes的cpu把最后几个地址称为向量
+// </summary>
 enum sfc_cpu_vector {
 	SFC_VECTOR_NMI=0xFFFA,
 	SFC_VECTOR_RESET=0xFFFC,
 	SFC_VECTOR_IRQBRK=0xFFFE,
 };
+
+//====================================
+//CPU寄存器
+class sfc_cpu_register_t {
+
+
+private:
+	//指令计数器
+	uint16_t program_counter;
+	//状态寄存器
+	uint8_t status;
+	//累加寄存器
+	uint8_t accumulator;
+	//X变址寄存器
+	uint8_t x_index;
+	//Y变址寄存器
+	uint8_t y_index;
+	//栈指针
+	uint8_t stack_pointer;
+	//保留对齐用
+	uint8_t unused;
+};
+
+
+//=====================
+//状态寄存器标志
+enum sfc_status_index {
+	SFC_INDEX_C = 0,
+	SFC_INDEX_Z = 1,
+	SFC_INDEX_I = 2,
+	SFC_INDEX_D=3,
+	SFC_INDEX_B=4,
+	SFC_INDEX_R=5,
+	SFC_INDEX_V=6,
+	SFC_INDEX_S=7,
+	SFC_INDEX_N=SFC_INDEX_S
+};
+
+//状态寄存器标记
+enum sfc_status_flag {
+	SFC_FLAG_C=1<<0,		//进位标记(Carry flag)
+	SFC_FLAG_Z=1<<1,		//零标记(Zero flag)
+	SFC_FLAG_I=1<<2,		//禁止中断(Irq disabled flag)
+	SFC_FLAG_D=1<<3,		//十进制模式(Decimal mode flag)
+	SFC_FLAG_B=1<<4,		//软件中断(BRK flag)
+	SFC_FLAG_R=1<<5,		//保留标记(Reserved)，一直为1
+	SFC_FLAG_V=1<<6,		//溢出标记(Overflow flag)
+	SFC_FLAG_S=1<<7,		//符号标记(Sign flag)
+	SFC_FLAG_N=SFC_FLAG_S	//又叫(Negative flag)
+};
+
