453f2e3114de2dbfdc3a1c7475784408cb16ab85
eclair-lumiere <1164983144l@gmail.com>
2019-05-26 14:28:49 +0800

    Update d2d_interface.h

--- fcEmulator/common/d2d_interface.h
+++ fcEmulator/common/d2d_interface.h
@@ -1,5 +1,5 @@
 ﻿#pragma once
-
+#include "../sfc_famicom_t.h"
 #ifndef SFC_NOEXCEPT
 #ifdef __cplusplus
 #define SFC_EXTERN_C extern "C" 
@@ -10,30 +10,9 @@
 #endif
 #endif
 
-SFC_EXTERN_C void main_cpp() SFC_NOEXCEPT;
-SFC_EXTERN_C void main_render(void* rgba) SFC_NOEXCEPT;
+SFC_EXTERN_C void main_cpp(sfc_famicom_t& famicom) SFC_NOEXCEPT;
+SFC_EXTERN_C void main_render(void* rgba,sfc_famicom_t& famicom) SFC_NOEXCEPT;
 SFC_EXTERN_C void user_input(int index, unsigned char data) SFC_NOEXCEPT;
 SFC_EXTERN_C int sub_render(void* rgba) SFC_NOEXCEPT;
 SFC_EXTERN_C void qsave() SFC_NOEXCEPT;
 SFC_EXTERN_C void qload() SFC_NOEXCEPT;
-
-
-#ifdef SFC_NO_INPUT
-SFC_EXTERN_C void user_input(int index, unsigned char data) SFC_NOEXCEPT {
-
-}
-#endif
-
-#ifdef SFC_NO_SUBRENDER
-SFC_EXTERN_C int sub_render(void* rgba) SFC_NOEXCEPT {
-    return 0;
-}
-#endif
-
-#ifdef SFC_NO_SL
-SFC_EXTERN_C void qsave() SFC_NOEXCEPT {
-}
-SFC_EXTERN_C void qload() SFC_NOEXCEPT {
-}
-#endif
-
