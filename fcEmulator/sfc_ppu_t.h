20900984e5f5b6e1edeb98db550c9164bac937be
eclair-lumiere <1164983144l@gmail.com>
2019-06-03 08:51:02 +0800

    Update sfc_ppu_t.h

--- fcEmulator/sfc_ppu_t.h
+++ fcEmulator/sfc_ppu_t.h
@@ -41,17 +41,47 @@ const union sfc_palette_data {
 	{ 0xA0, 0xFF, 0xF0, 0xFF },{ 0xA0, 0xA0, 0xA0, 0xFF },{ 0x00, 0x00, 0x00, 0xFF },{ 0x00, 0x00, 0x00, 0xFF }
 };
 
-// PPU用标志
-enum sfc_ppu_flag {
+/// PPU用标志 - $2000 - CTRL
+/// </summary>
+enum sfc_ppu_flag_2000 {
 	SFC_PPU2000_NMIGen = 0x80, // [0x2000]VBlank期间是否产生NMI
 	SFC_PPU2000_Sp8x16 = 0x20, // [0x2000]精灵为8x16(1), 还是8x8(0)
 	SFC_PPU2000_BgTabl = 0x10, // [0x2000]背景调色板表地址$1000(1), $0000(0)
-	SFC_PPUFLAG_SpTabl = 0x08, // [0x2000]精灵调色板表地址$1000(1), $0000(0), 8x16模式下被忽略
+	SFC_PPU2000_SpTabl = 0x08, // [0x2000]精灵调色板表地址$1000(1), $0000(0), 8x16模式下被忽略
 	SFC_PPU2000_VINC32 = 0x04, // [0x2000]VRAM读写增加值32(1), 1(0)
+};
 
+/// PPU用标志 - $2001 - MASK
+/// </summary>
+enum sfc_ppu_flag_2001 {
+	SFC_PPU2001_Grey = 0x01, // 灰阶使能
+	SFC_PPU2001_BackL8 = 0x02, // 显示最左边的8像素背景
+	SFC_PPU2001_SpriteL8 = 0x04, // 显示最左边的8像素精灵
+	SFC_PPU2001_Back = 0x08, // 背景显示使能
+	SFC_PPU2001_Sprite = 0x10, // 精灵显示使能
+
+	SFC_PPU2001_NTSCEmR = 0x20, // NTSC 强调红色
+	SFC_PPU2001_NTSCEmG = 0x40, // NTSC 强调绿色
+	SFC_PPU2001_NTSCEmB = 0x80, // NTSC 强调蓝色
+
+	SFC_PPU2001_PALEmG = 0x20, // PAL 强调绿色
+	SFC_PPU2001_PALEmR = 0x40, // PAL 强调红色
+	SFC_PPU2001_PALEmB = 0x80, // PAL 强调蓝色
+};
+
+
+/// PPU用标志 - $2002 - STATUS
+/// </summary>
+enum sfc_ppu_flag_2002 {
 	SFC_PPU2002_VBlank = 0x80, // [0x2002]垂直空白间隙标志
 	SFC_PPU2002_Sp0Hit = 0x40, // [0x2002]零号精灵命中标志
 	SFC_PPU2002_SpOver = 0x20, // [0x2002]精灵溢出标志
+};
+
+enum sfc_ppu_flag_sprite_attr {
+	SFC_SPATTR_FlipV = 0x80, // 垂直翻转
+	SFC_SPATTR_FlipH = 0x40, // 水平翻转
+	SFC_SPATTR_Priority = 0x20, // 优先位
 };
 
 class sfc_ppu_t
@@ -63,6 +93,10 @@ public:
 	//====================================
 	//内存地址库
 	uint8_t* banks[0x4000 / 0x0400];
+	//名称选择表(PPUCTRL低2位, 以及渲染中VRAM指针AB位)
+	uint8_t nametable_select;
+	//当前允许使用的垂直滚动偏移
+	uint8_t now_scrolly;
 	//VRAM地址
 	uint16_t vramaddr;
 	//寄存器 PPUCTRL	@$2000
@@ -82,8 +116,13 @@ public:
 	uint8_t pseudo;
 	//精灵调色板索引
 	uint8_t spindexes[0x20];
-	//精灵数据 :2568
-	uint8_t sprites[0x100];
+	//精灵数据 :256B -以32位对齐
+	union {
+		//以32位对齐
+		uint32_t aligned_buffer[0x100 / 4];
+		//数据
+		uint8_t sprites[0x100];
+	};
 
 	//====================================
 	//读取PPU地址
